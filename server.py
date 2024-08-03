b68bc704c828de6acab7130ff241d8a3f4c3cdbb
HeronErin <HeronErin@protonmail.com>
2024-08-03 14:04:12 -0400

    Make server loging more useful

--- server.py
+++ server.py
@@ -1,20 +1,31 @@
 from flask import Flask, request
-import os, json
+import os, json, time
 
 f = open("manifest.json", "r")
 manifest = json.loads(f.read())
+manifest["ipDict"] = manifest.get("ipDict", {})
 f.close()
 current = manifest.get("current", 0)
 
+
+def saveManifest():
+    global manifest
+    global current
+
+    manifest["current"] = current
+    f = open("manifest.json", "w")
+    f.write(json.dumps(manifest, indent = 4))
+    f.close()
+
 app = Flask(__name__)
-
-
 
 
 @app.route("/get_directive/<int:size>")
 def get_directive(size):
     global current
     current += size
+    manifest["ipDict"][request.remote_addr] = time.ctime()
+    saveManifest()
     return json.dumps([current-size, current])
 @app.route("/submit/<int:start>/<int:end>", methods = ["POST"])
 def submit(start, end):
@@ -32,7 +43,4 @@ if __name__ ==  "__main__":
     try:
         app.run()
     finally:
-        manifest["current"] = current
-        f = open("manifest.json", "w")
-        f.write(json.dumps(manifest))
-        f.close()
+        saveManifest()
